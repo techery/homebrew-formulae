5be43e4000b57a3b0cf0529f4ec3c53159316340
Sergey Zenchenko <sergeizenchenko@gmail.com>
2014-03-27 12:31:45 +0200

    liftoff: Release version 1.1.2

--- Formula/liftoff.rb
+++ Formula/liftoff.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Liftoff < Formula
   homepage 'https://github.com/techery/liftoff'
   url 'http://techery.github.io/liftoff/Liftoff-1.1.2.tar.gz'
-  sha1 'df836a64b6983f404e19e690c3d2d34ad79b37a2'
+  sha1 '4f3fd25d48188f06f4e1d02aa920c3897edf6f72'
 
   depends_on 'xcproj' => :recommended
 
